d1d3ec10ba5a944993b1629e7b589891e4b3a0da
Alan Ferreira <alan.ferreira@hotmail.com>
2022-04-04 22:40:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/alanferreira/workshopmongo/services/UserService.java
+++ src/main/java/com/alanferreira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alanferreira.workshopmongo.domain.User;
+import com.alanferreira.workshopmongo.dto.UserDTO;
 import com.alanferreira.workshopmongo.repository.UserRepository;
 import com.alanferreira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,6 +26,13 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
 	
 
