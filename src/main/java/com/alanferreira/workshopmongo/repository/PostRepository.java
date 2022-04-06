3fbd53b558d97926b987fb047393fc44de52a894
Alan Ferreira <alan.ferreira@hotmail.com>
2022-04-05 22:12:24 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/alanferreira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.alanferreira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alanferreira.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
