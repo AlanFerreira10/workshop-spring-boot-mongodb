00fefb8ffc635d8d7ffcb2b1cde79e05860c79a0
Alan Ferreira <alan.ferreira@hotmail.com>
2022-04-05 22:40:01 -0300

    Referenciando os posts do usuário

--- src/main/java/com/alanferreira/workshopmongo/domain/User.java
+++ src/main/java/com/alanferreira/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.alanferreira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /*@Document também funciona, o springData mapeia a coleção com o nome da classe com letra minuscula */
@@ -14,6 +17,10 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	/** lazy=true significa que será recuperado apenas os dados do User, só busca os dados do Post caso seja requisitado)**/
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User () {
 	}
@@ -49,6 +56,14 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,4 +88,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
 }
